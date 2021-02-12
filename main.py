041203e5b37aa8679ecbe38063c5e32c76ea846d
SzczepanJany <szczepan.jany@gmail.com>
2021-02-12 22:00:58 +0100

    computing the result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from random import randint
 
 def throws_and_dices():
     type_of_throw = input("Enter type of throw: ")
@@ -12,7 +13,24 @@ def throws_and_dices():
         sign = ""
     return (result,sign)
 
+def do_the_math(operants, operations):
+    throws = []
+    if operants[0] == '':
+        mulitple = 1
+    else:
+        mulitple = operants[0]
+
+    for _ in range(int(mulitple)):
+        throws.append(randint(1,int(operants[1])))
+        print(throws)
+    result = sum(throws)    
+    if operations == '-':
+        result = result - int(operants[2])
+    elif operations == '-':
+        result = result + int(operants[2])
+
+    return result
 
 
-
-throws_and_dices()
+operant, operation = throws_and_dices()
+print(do_the_math(operant, operation))
